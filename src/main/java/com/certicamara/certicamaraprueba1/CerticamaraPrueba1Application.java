4506bfeed3217f63c91099a42b9a76d31119add7
Angela Monsalve <angelamonsalve@wposs.com>
2022-02-18 14:02:17 -0500

    Archivos iniciales del proyecto

---
+++ src/main/java/com/certicamara/certicamaraprueba1/CerticamaraPrueba1Application.java
@@ -0,0 +1,13 @@
+package com.certicamara.certicamaraprueba1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerticamaraPrueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerticamaraPrueba1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/certicamara/certicamaraprueba1/CerticamaraPrueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certicamara.certicamaraprueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerticamaraPrueba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
