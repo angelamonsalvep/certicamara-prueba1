14b6a91a426eab7f14f7055e9bdac62dcf1a60d3
Angela Monsalve <angelamonsalve@wposs.com>
2022-02-19 09:32:44 -0500

    IMPLEMENTACIÓN - Controlador de Saludo inicial. Configuración de archivos properties para ambientes -dev y-pdn, y datos de conexión a base de datos local postgres para desarrollo.

---
+++ src/main/java/com/certicamara/certicamaraprueba1/web/controller/SaludoController.java
@@ -0,0 +1,15 @@
+package com.certicamara.certicamaraprueba1.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class SaludoController {
+
+    @GetMapping("/")
+    public String saludar(){
+        return "Bienvenido a mi API para Certicamara";
+    }
+}
