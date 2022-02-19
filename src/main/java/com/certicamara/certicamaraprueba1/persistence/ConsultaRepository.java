53bb97dba5dc573e2172e63e055cdd464e6d7da3
Angela Monsalve <angelamonsalve@wposs.com>
2022-02-19 10:44:37 -0500

    IMPLEMENTACIÓN - Se modifica el repositorio de Consulta para usar el mapper del dominio Query y desarrollo del metodo save.

--- src/main/java/com/certicamara/certicamaraprueba1/persistence/ConsultaRepository.java
+++ src/main/java/com/certicamara/certicamaraprueba1/persistence/ConsultaRepository.java
@@ -1,14 +1,26 @@
 package com.certicamara.certicamaraprueba1.persistence;
 
+import com.certicamara.certicamaraprueba1.domain.Query;
+import com.certicamara.certicamaraprueba1.domain.repository.QueryRepository;
 import com.certicamara.certicamaraprueba1.persistence.crud.ConsultaCrudRepository;
 import com.certicamara.certicamaraprueba1.persistence.entity.Consulta;
+import com.certicamara.certicamaraprueba1.persistence.mapper.QueryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ConsultaRepository {
+public class ConsultaRepository implements QueryRepository {
+
+    @Autowired
     private ConsultaCrudRepository consultaCrudRepository;
 
-    public Consulta save(Consulta consulta) {
-        return consultaCrudRepository.save(consulta);
+    @Autowired
+    private QueryMapper mapper;
+
+
+    @Override
+    public Query save(Query query) {
+        Consulta consulta = mapper.toConsulta(query);
+        return mapper.toQuery(consultaCrudRepository.save(consulta));
     }
 }
