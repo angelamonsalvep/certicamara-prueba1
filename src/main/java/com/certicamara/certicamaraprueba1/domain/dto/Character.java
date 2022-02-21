35d40cc300e95757cdc8bd025c89c38a383104e3
Angela Monsalve <angelamonsalve@wposs.com>
2022-02-20 19:03:42 -0500

    IMPLEMENTACIÓN - Se crea la clase Character para deserializar los datos obtenidos de la API externa que devuelve una lista de personajes de anime. Se crean los metodos consumirAPIExterna y depurardatostoLista.

---
+++ src/main/java/com/certicamara/certicamaraprueba1/domain/dto/Character.java
@@ -0,0 +1,82 @@
+package com.certicamara.certicamaraprueba1.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Character{
+
+    private String id;
+    private String name;
+    private String gender;
+    private String age;
+
+    @JsonProperty( "eye_color" )
+    private String eyeColor;
+
+    @JsonProperty( "hair_color" )
+    private String haircolor;
+
+    public Character() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getHaircolor() {
+        return haircolor;
+    }
+
+    public void setHaircolor(String haircolor) {
+        this.haircolor = haircolor;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age='" + age + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                ", haircolor='" + haircolor + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/certicamara/certicamaraprueba1/web/controller/QueryController.java
+++ src/main/java/com/certicamara/certicamaraprueba1/web/controller/QueryController.java
@@ -1,6 +1,7 @@
 package com.certicamara.certicamaraprueba1.web.controller;
 
 import com.certicamara.certicamaraprueba1.domain.Query;
+import com.certicamara.certicamaraprueba1.domain.dto.Character;
 import com.certicamara.certicamaraprueba1.domain.service.QueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -19,9 +21,9 @@ public class QueryController {
     @Autowired
     private QueryService queryService;
 
-    @GetMapping("/people")
-    public ResponseEntity<List<Object>> getCats() {
-        List<Object> people = consumirAPIexterna();
+    @GetMapping("/character")
+    public ResponseEntity<String> getCharacter() {
+        String nameCharacter = depurarDatostoString();
         Query query = new Query();
         queryService.save(query);
         try{
@@ -29,25 +31,67 @@ public class QueryController {
         } catch (Exception e){
             e.printStackTrace();
         }
-        return new ResponseEntity<>(people, HttpStatus.OK);
+        return new ResponseEntity<>(nameCharacter, HttpStatus.OK);
     }
+
+
+    @GetMapping("/people")
+    public ResponseEntity<List<Character>> getPeople() {
+        List<Character> listaDepurada = depurarDatostoLista();
+        Query query = new Query();
+        queryService.save(query);
+        try{
+            queryService.save(query);//se guarda en la base de datos la consulta realizada a la API externa.
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(listaDepurada, HttpStatus.OK);
+    }
+
+
+    /**
+     * Método que crea un objeto tipo Character desde una lista .
+     * @return un String con el nombre del Character
+     */
+    private String depurarDatostoString() {
+        List<Character> characterList = consumirAPIExterna();
+        Character character;
+        character = characterList.get(1);
+        return character.getName();
+
+    }
+
+    /**
+     * Método que llena una lista de objetos tipo Character con datos depurados
+     * @return lista de objetos tipo character
+     */
+    private List<Character> depurarDatostoLista() {
+        List<Character> characterList = consumirAPIExterna();
+        List<Character> listaDepurada = new ArrayList<>();
+
+        int i;
+        for(i = 0; i < characterList.size(); i++) {
+            Character character;
+            character = characterList.get(i);
+            listaDepurada.add(character);
+        }
+         return listaDepurada;
+    }
+
 
     /**
      * Método que consume una API externa que obtiene un listado de datos(people)
      * @return lista de objetos tipo character
      */
-    private List<Object> consumirAPIexterna() {
-        Object[] character = null;
-        try {
-            String url= "https://ghibliapi.herokuapp.com/people";
-            RestTemplate restTemplate = new RestTemplate();
-
-            character= restTemplate.getForObject(url, Object[].class);
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        return Arrays.asList(Objects.requireNonNull(character));
+    public List<Character> consumirAPIExterna(){
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Character[]> response =
+                restTemplate.getForEntity(
+                        "https://ghibliapi.herokuapp.com/people",
+                        Character[].class);
+        Character[] characters = response.getBody();
+        return Arrays.asList(Objects.requireNonNull(characters));
     }
+
 
 }
